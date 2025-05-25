7c59c27a8e587c79387d7ddc568d2fcb79d979e8
maltesaa13@gmail.com <maltesaa13@gmail.com>
2025-05-25 20:51:25 +0200

    klar

--- main.c
+++ main.c
@@ -8,6 +8,33 @@ int main()
 
    
     // Your code goes here
+    const int numberOfRolls = 100;
+
+    int rolls[numberOfRolls];
+    int occurrences[] = {0,0,0,0,0,0};
+
+    for (int i=0; i<numberOfRolls; i++){
+        rolls[i] = (rand() %6) + 1;
+        occurrences[rolls[i]-1]++;
+    }
+    
+    //Print number of occurrences per number
+    for (int i=0; i<6; i++){
+       printf("%d\n", occurrences[i]);
+    }
+
+    //Calculate and print sum of rolls
+    float sum = 0;
+
+    for (int i=0; i<numberOfRolls; i++){
+        sum+=rolls[i];
+    }
+    printf("%.f\n", sum);
+
+    //Calculate and print the average roll
+    float avg = (sum / numberOfRolls);
+
+    printf("%.1f\n", avg);
 
     return 0;
 }
